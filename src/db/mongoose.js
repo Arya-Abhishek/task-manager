8479cfdd40ffa971093d315a5c9d76bd8f1fcefc
Aryan-Abhishek <arya2001abhishek@gmail.com>
2020-02-21 10:00:53 +0530

    more validations added for password!

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,69 +7,88 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useUnifiedTopology: true
 })
 
-//model created 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        reqired: true,
+// //model created 
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//         reqired: true,
 
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid!')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        dafault: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number!')
-            } 
-        }
-    }
-})
+//         trim: true
+//     },
+//     password: {
+//         type: String,
+//         trim: true,
+//         required: true,
+//         validate(value) {
+//             if (!validator.isLength(value, { min: 6 })) { 
+//                 throw new Error('provide the length of password grater than 6')
+//             }
 
-// instance created
-const me = new User({
-    name: '  Mike   ',
-    email: '    NEWMAIL@GITHUB.IO  ',
-    age: 28
-})
-
-//nothing will happen until here until we console log or save it to our database
-
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
-})
-
-//Another model creation: Tasks
-// const Tasks = mongoose.model('Tasks', {
-//     description: {
-//         type: String
-//     }, 
-//     completed: {
-//         type: Boolean
+//             if (validator.contains(value, 'password')) {
+//                 throw new Error('password must not contain string password!')
+//             }
+//         }
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value) {
+//             if (!validator.isEmail(value)) {
+//                 throw new Error('Email is invalid!')
+//             }
+//         }
+//     },
+//     age: {
+//         type: Number,
+//         dafault: 0,
+//         validate(value) {
+//             if (value < 0) {
+//                 throw new Error('Age must be a positive number!')
+//             } 
+//         }
 //     }
 // })
 
-// instance creation for saving it to database
-// const task_1 = new Tasks({
-//     description: 'water the plants',
-//     completed: false
+// // instance created
+// const me = new User({
+//     name: '  Nike   ',
+//     password: ' nikpassCode123   ',
+//     email: '    NEW1234MAIL@GITHUB.IO  ',
+//     age: 28
 // })
 
-// task_1.save().then(() => {
-//     console.log(task_1)
+//nothing will happen until here until we console log or save it to our database
+
+// me.save().then(() => {
+//     console.log(me)
 // }).catch((error) => {
 //     console.log('Error!', error)
 // })
+
+//Another model creation: Tasks
+const Tasks = mongoose.model('Tasks', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    }, 
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+//instance creation for saving it to database
+const task_1 = new Tasks({
+    description: '    wash the car',
+    completed: true
+    
+})
+
+task_1.save().then(() => {
+    console.log(task_1)
+}).catch((error) => {
+    console.log('Error!', error)
+})
