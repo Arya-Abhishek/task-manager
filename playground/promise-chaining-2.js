1255081c3eff188175836742fb39fb055595bbf5
Aryan-Abhishek <arya2001abhishek@gmail.com>
2020-02-29 01:19:44 +0530

    explored RESTful api

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,27 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5e4f5892d71db146a53e93f1').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: true })
-}).then((result) => {
+// Task.findByIdAndDelete('5e4f5892d71db146a53e93f1').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: true })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const incompleteTasks = await Task.countDocuments({ completed: false })
+    return {
+        task: task,
+        incompleteTasks: incompleteTasks
+    }
+}
+
+deleteTaskAndCount('5e4f58b5af4b3946b685a090').then((result) => {
     console.log(result)
+    console.log(result.incompleteTasks)
 }).catch((e) => {
     console.log(e)
 })
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,27 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5e57e16ae6ba5d19ea6a3345', { age: 1 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 1 })
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate('5e57e16ae6ba5d19ea6a3345', { age: 1 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return {
+        user: user,
+        count: count
+    }
+}
+
+updateAgeAndCount('5e57e3382131cf1cf94d9902', 21).then((result) => {
+    console.log(result.user)
+    console.log(result.count)
 }).catch((e) => {
     console.log(e)
 })
