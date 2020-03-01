e7ba71e65a7285dbb201b7028fcc174a11dc6611
Aryan-Abhishek <arya2001abhishek@gmail.com>
2020-03-01 17:07:24 +0530

    Saving my work, just in case

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -12,7 +12,7 @@ const auth = async (req, res, next) => {
             throw new Error()
         }
 
-        req.token = token
+        req.token = tokengti
         req.user = user
         next()
     } catch (e) {
