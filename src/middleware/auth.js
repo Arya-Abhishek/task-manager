988a2fbd4a27c0130edb39892191216d8cb4e6bc
Aryan-Abhishek <arya2001abhishek@gmail.com>
2020-03-03 21:44:17 +0530

    completed user model and added completed user authorization

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -12,7 +12,7 @@ const auth = async (req, res, next) => {
             throw new Error()
         }
 
-        req.token = tokengti
+        req.token = token
         req.user = user
         next()
     } catch (e) {
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,104 +1,3 @@
-// const express = require('express')
-// const router = new express.Router()
-// const User = require('../models/user')
-// const auth = require('../middleware/auth')
-
-// router.post('/users/login', async (req, res) => {
-//     try {
-//         const user = await User.findByCredentials(req.body.email, req.body.password)
-//         const token = await user.generateAuthToken()
-//         res.send({ user, token })
-//     } catch (e) {
-//         res.status(400).send()
-//     }
-// })
-
-// router.post('/users', async (req,res) => {
-//     const user = new User(req.body)
-
-//     try {
-//         await user.save()
-
-//         const token = await user.generateAuthToken()
-
-//         res.status(201).send({ user, token })
-//     } catch (e) {
-//         res.status(400).send()
-//     }
-// })
-
-
-// router.get('/users/:id', async (req,res) => {
-    
-//     const _id = req.params.id   
-
-//     try {
-//         // accessing particular user by its id using async and await
-//         const user = await User.findById(_id)
-//         if (!user) {
-//             return res.status(404).send()
-//         }
-
-//         res.send(user)
-//     } catch (e) {
-//         res.status(500).send()
-//         //note 500 represents internal server error refer statuses.com
-//     }
-
-// })
-
-
-// router.get('/users/me', auth, async (req, res) => {
-//     res.send(req.user)
-// })
-
-// router.patch('/users/:id', async (req, res) => {
-
-//     const updates = Object.keys(req.body)
-//     const allowedUpdates = ['name', 'email', 'password', 'age']
-//     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-//     if (!isValidOperation) {
-//         return res.status(400).send({ error: 'Invalid updates!' })
-//     }
-
-//     try {
-//         //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
-//         const user = await User.findById(req.params.id)
-
-//         updates.forEach((update) => user[update] = req.body[update])
-//         await user.save()
-
-//         if (!user) {
-//             return res.status(404).send()
-//         }
-
-//         res.send(user)
-//     } catch (e) {
-//         res.status(400).send()
-//     }
-// })
-
-// router.delete('/users/:id', async (req, res) => {
-
-//     try {
-//         const user = await User.findByIdAndDelete(req.params.id)
-
-//         if (!user) {
-//             res.status(404).send()
-//         }
-
-//         res.send(user)
-//     } catch (e) {
-//         res.status(500).send()
-//     }
-
-// })
-
-
-// module.exports = router
-
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
@@ -128,7 +27,7 @@ router.post('/users/login', async (req, res) => {
 
 router.post('/users/logout', auth, async (req, res) => {
     try {
-        req.user.tokens =req.user.tokens.filter((token) => {
+        req.user.tokens = req.user.tokens.filter((token) => {
             return token.token !== req.token
         })
 
@@ -139,27 +38,25 @@ router.post('/users/logout', auth, async (req, res) => {
     }
 })
 
-router.get('/users/me', auth, async (req, res) => {
-    res.send(req.user)
-})
-
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
+router.post('/users/logoutAll', auth, async (req, res) => {
     try {
-        const user = await User.findById(_id)
 
-        if (!user) {
-            return res.status(404).send()
-        }
+        //now we need to wipe all token, to ensure complete logout of user
+        req.user.tokens = []
 
-        res.send(user)
+        await req.user.save()
+        res.status(200).send()
     } catch (e) {
         res.status(500).send()
     }
 })
 
-router.patch('/users/:id', async (req, res) => {
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
+})
+
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -169,14 +66,16 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findById(req.params.id)
+        // const user = await User.findById(req.params.id)
+        const user = req.user
 
         updates.forEach((update) => user[update] = req.body[update])
         await user.save()
 
-        if (!user) {
-            return res.status(404).send()
-        }
+        // if (!user) {
+        //     return res.status(404).send()
+        // }
+        // Above code is not required as the user is here it means he/she just passed the authentication
 
         res.send(user)
     } catch (e) {
@@ -184,15 +83,16 @@ router.patch('/users/:id', async (req, res) => {
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
+        // const user = await User.findByIdAndDelete(req.user._id)
 
-        if (!user) {
-            return res.status(404).send()
-        }
+        // if (!user) {
+        //     return res.status(404).send()
+        // }
+        await req.user.remove()
 
-        res.send(user)
+        res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
